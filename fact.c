536473501b88158f32358cb591ad91d8d0eaa882
kumarvirendra186 <87355368+kumarvirendra186@users.noreply.github.com>
2021-07-17 21:24:09 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,7 +2,7 @@
 void factorial()    
 {    
  int i,fact=1,number;    
- printf("\nEnter  number: ");    
+ printf("\nEnter a number: ");    
   scanf("%d",&number);    
     for(i=1;i<=number;i++){    
       fact=fact*i;    
